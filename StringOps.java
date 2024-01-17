e36dfeb573f25cac2a05394b45288fec804b70e2
guymend <guy.mendelson@post.runi.ac.il>
2024-01-17 12:11:20 +0200

    StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -29,21 +29,86 @@ public class StringOps {
         for(int i = 0; i < string.length(); i++){
             Character letter =  string.charAt(i);
             if((((int) letter) > 64) && (((int) letter) < 91)){
-                string = string.replace(string.charAt(i), (char)(((int) letter) + 32));
+                if(i == 0){
+                    string = (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                }else if ( i == string.length()){
+                    string = string.substring(0, i) + (char)(((int) letter) + 32);
+                }else{
+                    string = string.substring(0, i) + (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                }
             }
         }
         for(int i = 0; i < string.length(); i++){
             Character letter =  string.charAt(i);
             if((((int) letter) == 97) || (((int) letter) == 101) || (((int) letter) == 105) || (((int) letter) == 111) || (((int) letter) == 117)){
-                string = string.replace(string.charAt(i), (char)(((int) letter) - 32));
+                if(i == 0){
+                    string = (char)(((int) letter) - 32) + string.substring(i+1,string.length());
+                }else if ( i == string.length()){
+                    string = string.substring(0, i) + (char)(((int) letter) - 32);
+                }else{
+                    string = string.substring(0, i) + (char)(((int) letter) - 32) + string.substring(i+1,string.length());
+                }
             } 
         }
         return string;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        int wordCounter = 0;
+        int letterInWord = 0;
+        for(int i = 0; i < string.length(); i++){
+            Character letter =  string.charAt(i);
+            
+            if((int)(string.charAt(i)) == 32){
+                wordCounter++;
+                letterInWord = 0;
+                if(i == 0){
+                    string = string.substring(i+1,string.length());
+                }else{
+                    string = string.substring(0, i) + string.substring(i+1,string.length());
+                }
+                i = i-1;
+            }else{
+                if(wordCounter == 0){
+                    if((((int) letter) > 64) && (((int) letter) < 91)){
+                        if(i == 0){
+                            string = (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                        }else if ( i == string.length()){
+                            string = string.substring(0, i) + (char)(((int) letter) + 32);
+                        }else{
+                            string = string.substring(0, i) + (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                        }
+                    }
+                }else{
+                    if(letterInWord == 0){
+                        if((((int) letter) > 97) && (((int) letter) < 122)){ 
+                            if(i == 0){
+                                string = (char)(((int) letter) - 32) + string.substring(i+1,string.length());
+                            }else if ( i == string.length()){
+                                string = string.substring(0, i) + (char)(((int) letter) - 32);
+                            }else{
+                                string = string.substring(0, i) + (char)(((int) letter) - 32) + string.substring(i+1,string.length());
+                            }
+                        }
+                    }else{
+                        if((((int) letter) > 64) && (((int) letter) < 91)){
+                            if(i == 0){
+                                string = (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                            }else if ( i == string.length()){
+                                string = string.substring(0, i) + (char)(((int) letter) + 32);
+                            }else{
+                                string = string.substring(0, i) + (char)(((int) letter) + 32) + string.substring(i+1,string.length());
+                            }
+                        }
+                    }
+                    letterInWord++;
+                }
+
+                
+            }
+
+        }
+        return string;
     }
 
     public static int[] allIndexOf (String string, char chr) {
